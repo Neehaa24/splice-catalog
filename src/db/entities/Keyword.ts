26005d51c461a06b34f5d0fbcef2f3a9a0ef4267
Neehaa24 <neehaa24@vt.edu>
2025-08-10 22:18:07 -0400

    Update Keyword.ts

--- src/db/entities/Keyword.ts
+++ src/db/entities/Keyword.ts
@@ -1,12 +1,13 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
-@Entity()
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+@Entity({ name: "keywords" })
 export class Keyword {
   @PrimaryGeneratedColumn()
-  id: number;
+  id!: number;
 
-@Column()
-name: string;
+  @Column({ type: "varchar", length: 200 })
+  name!: string;
 
-@Column({ nullable: true})
-description: string;
+  @Column({ type: "text", nullable: true })
+  description!: string | null;
 }
